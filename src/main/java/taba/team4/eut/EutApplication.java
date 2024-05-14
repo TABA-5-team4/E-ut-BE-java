86f65d55ed007e5fd3ae4545e9b0bebc7ffa4f1f
kwon416 <bbbggg111@naver.com>
2024-05-14 12:54:06 +0900

    first commit

---
+++ src/main/java/taba/team4/eut/EutApplication.java
@@ -0,0 +1,13 @@
+package taba.team4.eut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taba/team4/eut/EutApplicationTests.java
@@ -0,0 +1,13 @@
+package taba.team4.eut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
